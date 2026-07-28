48b11ed6a6f337a030b952486431c737c24cb98f
Dat <dathoang9797@gmail.com>
2026-07-28 13:47:52 +0700

    up

--- Lab/src/Domain/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright © Công Ty Cổ Phần Đầu Tư Công Nghệ Thiên An
- * Website: http://tacorp.vn
- */
-
-global using System;
-global using System.Collections.Generic;
-global using System.ComponentModel.DataAnnotations;
